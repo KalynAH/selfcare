78d77145f1f694e06777b5b37475f5a94f95c34f
Kalyn Huff <huffkalyn@yahoo.com>
2024-06-26 12:20:11 -0400

    second commit

--- server/flask_app/controllers/selfcares.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify, make_response, request
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt
-from flask_app.models.user import User
-from flask_app.extensions import jwt
-from flask_app import bcrypt
-
-
-auth = Blueprint("auth", __name__)
