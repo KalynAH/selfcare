adcc59608059c4d0433414a6ce8fffde2590a806
Kalyn Huff <huffkalyn@yahoo.com>
2024-07-01 14:57:58 -0400

    initial commit

---
+++ client/src/utils/WithAuth.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+function WithAuth(Component) {
+  return (props) => {
+    const { isAuthenticated } = useContext(AuthContext);
+    if (!isAuthenticated) {
+      return <Navigate to="/login?message=unauthorized" />;
+    }
+    return <Component {...props} />;
+  };
+}
+export default WithAuth;
