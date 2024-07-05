0d7258a121b20ad000f87159f16c85b8293c1afb
Kalyn Huff <huffkalyn@yahoo.com>
2024-07-05 09:14:59 -0400

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,38 @@
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthProvider";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import LoginForm from "./components/LoginForm";
 import Navbar from "./components/Navbar";
-import { editSelfcare } from "./services/selfcare_service";
-import OneSelfcare from "./components/OneSelfcare";
-import NewSelfcare from "./components/NewSelfcare";
-import EditSelfcare from "./components/EditSelfcare";
+import ProtectedEditSelfcare from "./protected/ProtectedEditSelfcare";
+import ProtectedNewSelfcare from "./protected/ProtectNewSelfcare";
+import ProtectedOneSelfcare from "./protected/ProtectedOneSelfcare";
 import RegisterForm from "./components/RegisterForm";
-import AuthProvider from "./context/AuthContext";
 
 import ProtectedAllSelfcares from "./components/ProtectedAllSelfcares";
+import UserBar from "./components/Userbar";
 
 function App() {
+  const { isAuthenticated } = useContext(AuthContext);
   return (
-    <AuthProvider>
+    <>
       <Navbar />
+      {isAuthenticated && <UserBar />}
       <main className="container py-3">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/selfcares" element={<ProtectedAllSelfcares />} />
-          <Route path="/selfcares/:id" element={<OneSelfcare />} />
-          <Route path="/selfcares/new" element={<NewSelfcare />} />
+          <Route path="/selfcares/:id" element={<ProtectedOneSelfcare />} />
+          <Route path="/selfcares/new" element={<ProtectedNewSelfcare />} />
           <Route
             path="/selfcares/:id/edit"
-            element={<EditSelfcare />}
-            submitFunction={editSelfcare}
+            element={<ProtectedEditSelfcare />}
           />
         </Routes>
       </main>
-    </AuthProvider>
+    </>
   );
 }
 
