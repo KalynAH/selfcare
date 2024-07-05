948161b50857a65ed007d94e9abf2a52502015c7
Kalyn Huff <huffkalyn@yahoo.com>
2024-07-05 09:24:01 -0400

    updated

--- server/flask_app/models/selfcare.py
+++ server/flask_app/models/selfcare.py
@@ -1,11 +1,23 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from datetime import datetime
+from flask_app.models.user import User
+from pprint import pprint
 
 
 class Selfcare:
     """Represents a selfcare routine"""
 
     _db = "selfcares_db"
+
+    def __init__(self, data):
+        self.id = data["id"]
+        self.title = data["title"]
+        self.description = data["description"]
+        self.location = data["location"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+        self.user_id = data["user_id"]
+        self.user = None
 
     @staticmethod
     def validate_selfcare(data):
@@ -34,7 +46,7 @@ class Selfcare:
     @classmethod
     def find_all(cls):
         """Finds all the selfcares in the database"""
-        query = "SELECT *FROM selfcares;"
+        query = "SELECT * FROM selfcares;"
         return connectToMySQL(Selfcare._db).query_db(query)
 
     @classmethod
@@ -43,9 +55,9 @@ class Selfcare:
 
         query = """
         INSERT INTO selfcares
-        (title, description, location)
+        (title, description, location, user_id)
         VALUES
-        (%(title)s, %(description)s, %(location)s);
+        (%(title)s, %(description)s, %(location)s, %(user_id)s);
         """
         connectToMySQL(Selfcare._db).query_db(query, form_data)
         return
@@ -62,6 +74,34 @@ class Selfcare:
         return list_of_dicts[0]
 
     @classmethod
+    def find_by_id_with_user(cls, users_id):
+        """This methods finds a user and a selfcare by selfcare id"""
+        query = """
+        SELECT * FROM users
+        LEFT JOIN selfcares
+        ON selfcares.user_id = users.id
+        WHERE users.id = %(users_id)s;
+        """
+        data = {"id": users_id}
+        list_of_dicts = connectToMySQL(Selfcare._db).query_db(query, data)
+        pprint(list_of_dicts)
+        user = User(list_of_dicts[0])
+
+        for one_dict in list_of_dicts:
+
+            selfcare_data = {
+                "id": one_dict["selfcares.id"],
+                "title": one_dict["title"],
+                "description": one_dict["description"],
+                "location": one_dict["location"],
+                "created_at": one_dict["selfcares.created_at"],
+                "updated_at": one_dict["selfcares.updated_at"],
+            }
+            user.selfcares.append(Selfcare(selfcare_data))
+
+        return user
+
+    @classmethod
     def update(cls, selfcare_id, form_data):
         """Updates a selfcare routine from a form."""
 
@@ -70,7 +110,7 @@ class Selfcare:
         SET
         title=%(title)s,
         description=%(description)s,
-        location=%(location)s,
+        location=%(location)s
         WHERE id = %(selfcare_id)s;
         """
         form_data["selfcare_id"] = selfcare_id
@@ -84,3 +124,4 @@ class Selfcare:
         query = "DELETE FROM selfcares WHERE id = %(selfcare_id)s;"
         data = {"selfcare_id": selfcare_id}
         connectToMySQL(Selfcare._db).query_db(query, data)
+        return
